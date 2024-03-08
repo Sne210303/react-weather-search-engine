0a52797793b770da120f917f6067d955a957f1a3
Sne210303 <snenhlanhlamkhwanazi24@gmail.com>
2024-03-08 21:08:43 +0200

    Added some files.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from "./logo.svg";
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Port Harcourt" />
+        <footer className="footer-link">
+          This project was coded by{" "}
+          <a href="https://github.com/bobjoan" target="_blank" rel="noreferrer">
+            Joan Bob
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/bobjoan/weather-practice"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
 export default App;
